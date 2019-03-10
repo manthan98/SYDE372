c80fb5b5317e4af8c08fbabe97b2a6718f8a2507
manthan98 <manthan711@gmail.com>
2019-03-10 16:31:38 -0400

    Pretty code

--- Lab 2/Gaussian1D.m
+++ Lab 2/Gaussian1D.m
@@ -3,18 +3,10 @@ function [ mu, sigma ] = Gaussian1D( x )
 % set of samples assumed to be normally distributed.
 
     size_x = length(x);
-    mu = 0;
-    sigma = 0;
     
-    for i = 1:size_x
-        mu = mu + x(i);
-    end
-    mu = (1 / size_x) * mu;
+    mu = (1 / size_x) * sum(x);
     
-    for i = 1:size_x
-        sigma = sigma + (x(i) - mu)^2;
-    end
-    sigma = (1 / size_x) * sigma;
+    sigma = (1 / size_x) * sum((x - mu).^2);
     sigma = sqrt( ( size_x / (size_x - 1) ) * sigma );
 
 end
