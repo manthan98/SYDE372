e9f8f09654c25cd772e47e569ba35a34ec668d74
Austin Atmaja <austin.atmaja@gmail.com>
2019-03-30 16:05:35 -0400

    changed var naming

--- Lab 2/ML2D.m
+++ Lab 2/ML2D.m
@@ -9,9 +9,9 @@ function points = ML2D(X, Y, a, b, c)
     sigma_B = (1/length(b))*(b'*b) - mu_B'*mu_B;
     sigma_C = (1/length(c))*(c'*c) - mu_C'*mu_C;
     
-    MAP_AB = MLHelper(X, Y, mu_A, sigma_A, mu_B, sigma_B);
-    MAP_BC = MLHelper(X, Y, mu_B, sigma_B, mu_C, sigma_C);
-    MAP_CA = MLHelper(X, Y, mu_C, sigma_C, mu_A, sigma_A);
+    ML_AB = MLHelper(X, Y, mu_A, sigma_A, mu_B, sigma_B);
+    ML_BC = MLHelper(X, Y, mu_B, sigma_B, mu_C, sigma_C);
+    ML_CA = MLHelper(X, Y, mu_C, sigma_C, mu_A, sigma_A);
 
     points = zeros(size(X, 1), size(Y, 2));
 
@@ -21,11 +21,11 @@ function points = ML2D(X, Y, a, b, c)
 
     for i = 1:size(X, 1)
         for j = 1:size(Y, 2)
-            if MAP_AB(i, j) >= 0 && MAP_BC(i, j) <= 0
+            if ML_AB(i, j) >= 0 && ML_BC(i, j) <= 0
                 points(i, j) = d;
-            elseif MAP_AB(i, j) <= 0 && MAP_CA(i, j) >= 0
+            elseif ML_AB(i, j) <= 0 && ML_CA(i, j) >= 0
                 points(i, j) = c;
-            elseif MAP_BC(i, j) >= 0 && MAP_CA(i, j) <= 0
+            elseif ML_BC(i, j) >= 0 && ML_CA(i, j) <= 0
                 points(i, j) = e;
             else
                 % wut
