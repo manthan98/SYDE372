91886f0412ef22722aef101076f0a7d09d2b2cb1
David Zhang <dbzhang@uwaterloo.ca>
2019-04-01 16:53:52 -0400

    stuff

--- Lab 3/lab3.m
+++ Lab 3/lab3.m
@@ -1,4 +1,17 @@
 addpath('./data');
 load feat.mat;
 
-aplot(face);
+%aplot(face);
+
+resf2 = SplitArrays(f2);
+resf8 = SplitArrays(f8);
+resf32 = SplitArrays(f32);
+
+mu = zeros(1,2,10);
+sigma = zeros(2,2,10);
+
+for i = 1:10
+    mu(:,:,i) = mean(resf2(:,:,i));
+    sigma(:,:,i) = cov(resf2(:,:,i));
+end
+
