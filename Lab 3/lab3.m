7a3869073bde918df1c6cb90ccfa380043e634d8
heathkd <heather.dsouza@hotmail.com>
2019-04-03 14:58:45 -0400

    part 5

--- Lab 3/lab3.m
+++ Lab 3/lab3.m
@@ -84,3 +84,60 @@ figure;
 imagesc(multim);
 xlabel('x_{1}');
 ylabel('x_{2}');
+
+%% Unlabelled Clustering
+
+features = f32(1:2,:);
+prototypeCandidates = features;
+
+% randomly choose initial prototypes
+for i = 1:10
+    index = randi([1 size(prototypeCandidates,2)]);
+    prototypes(1:2,i) = prototypeCandidates(:,index);
+    prototypeCandidates(:,index) = [];
+end
+
+% repeat classification until prototypes become stable
+while (~isequal(prev_prototypes,prototypes))
+    % classify each feature point
+    classifications = features;
+    for i = 1:length(features) 
+        point = features(:,i);
+        minDistance = realmax;
+        for j = 1:10
+            distance = sqrt((point(1)- prototypes(1,j)).^2 + (point(2)- prototypes(2,j)).^2);
+            if (distance < minDistance) 
+                minDistance = distance;
+                closestPrototype = j;
+            end
+        end
+        classifications(3,i) = closestPrototype;
+    end
+
+    prev_prototypes = prototypes;
+    % calculate  new prototypes
+    for i = 1:10
+        logicals = classifications(3,:) == i;
+        classPoints = classifications(:,logicals);
+        prototypes(1,i) = mean(classPoints(1,:));
+        prototypes(2,i) = mean(classPoints(2,:));
+    end
+end
+
+% plot
+figure
+hold on
+colours = {'Blue', 'Cyan', 'Green', 'Magenta', 'Red', [0 0.4470 0.7410], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560], [0.3010 0.7450 0.9330], [0.4660 0.6740 0.1880]};
+for i = 1:10
+    logicals = classifications(3,:) == i;
+    classPoints = classifications(:,logicals);
+    x = classPoints(1,:);
+    y = classPoints(2,:);
+    scatter(x,y,'MarkerFaceColor',colours{i},'MarkerEdgeColor', 'none');
+    scatter(prototypes(1,i),prototypes(2,i),'MarkerFaceColor',colours{i},'MarkerEdgeColor', 'black', 'LineWidth', 2, 'HandleVisibility','off');
+end
+legend('Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7', 'Class 8', 'Class 9', 'Class 10','Location', 'northeastoutside')
+xlabel('x_{1}');
+ylabel('x_{2}');
+
+
